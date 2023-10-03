18aaa4190db3050090b7a6fac6472fd940efa9cc
Jack <wj.jackli0327@gmail.com>
2023-10-03 16:04:29 -0500

    home page display items with api

--- src/views/Gadget/Home.vue
+++ src/views/Gadget/Home.vue
@@ -4,28 +4,76 @@
       <h1 class="banner-title">Steam Inventory Exchange</h1>
     </div>
 
-    <div class="sections">
-      <div class="section">
-        <h2 class="section-name">Popular</h2>
-        <router-link to="/Gadget/Market" class="detail-link"
-          >Enter Market</router-link
-        >
-        <div class="item-container">
-          <!-- Two rows of five items each go here -->
-          <!-- Example item structure for one item -->
-          <div class="item">
-            <img src="item1.jpg" alt="Item 1" class="item-image" />
-            <p class="item-description">Description of Item 1</p>
+    <div>
+      <div
+        class="section-container"
+        v-for="(section, index) in groupedItems"
+        :key="index"
+      >
+        <section class="item-section">
+          <header class="section-header">
+            <span class="section-name">{{ 'Popular Items' }}</span>
+            <a class="market-link" href="/market" target="_blank">Market</a>
+          </header>
+          <div class="row">
+            <div v-for="(item, itemId) in section" :key="itemId" class="col">
+              <div class="item-card">
+                <img :src="item.image" :alt="item.name" />
+                <p>{{ item.name }}</p>
+                <p>{{ item.price }}</p>
+                <a :href="item.link" target="_blank">View on Steam</a>
+              </div>
+            </div>
           </div>
-          <!-- Repeat the above structure for other items -->
-        </div>
+        </section>
       </div>
-      <!-- Repeat the above section structure for other sections -->
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script>
+export default {
+  data() {
+    return {
+      items: null
+    }
+  },
+  computed: {
+    groupedItems() {
+      if (!this.items) return []
+
+      // Convert the object into an array of items
+      const itemsArray = Object.values(this.items)
+
+      // Split the items into groups of 5
+      const grouped = []
+      for (let i = 0; i < itemsArray.length; i += 5) {
+        grouped.push(itemsArray.slice(i, i + 5))
+      }
+
+      // Limit to 5 sections
+      return grouped.slice(0, 5)
+    }
+  },
+  methods: {
+    sectionName(index) {
+      // Generate section name based on the index (customize as needed)
+      return `Section ${index + 1}`
+    }
+  },
+  mounted() {
+    // Fetch data from the API
+    fetch('http://66.11.119.83:8423/getItems')
+      .then((response) => response.json())
+      .then((data) => {
+        this.items = data
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }
+}
+</script>
 
 <style scoped>
 .home {
@@ -43,6 +91,7 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 20px;
 }
 
 .banner-title {
@@ -50,63 +99,51 @@
   margin: 0;
 }
 
-.sections {
-  /* Add styles for the sections container */
+.section-container {
   display: flex;
-  flex-wrap: wrap;
+  justify-content: center;
 }
 
-.section {
-  /* Add styles for each section */
-  width: calc(100% - 20px); /* Two sections per row with some spacing */
-  margin: 10px;
-  padding: 20px;
+.item-section {
+  width: 80%;
   border: 1px solid #ccc;
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
+  margin-bottom: 20px;
+  padding: 10px;
   position: relative;
 }
 
 .section-header {
-  position: absolute;
-  top: 10px; /* Adjust the top distance as needed */
-  left: 10px; /* Adjust the left distance as needed */
   display: flex;
   justify-content: space-between;
-  align-items: center;
 }
 
 .section-name {
-  /* Add styles for section names in the top left corner */
+  font-weight: bold;
 }
 
-.detail-link {
-  /* Add styles for detail page links in the top right corner */
-  color: #007bff; /* Example link color */
+.market-link {
+  text-decoration: none;
+  color: blue;
 }
 
-.item-container {
-  /* Add styles for the container of items within a section */
+.row {
   display: flex;
-  flex-wrap: wrap;
   justify-content: space-between;
+  margin-top: 10px;
 }
 
-.item {
-  /* Add styles for each item */
-  width: calc(20% - 10px); /* Five items per row with some spacing */
-  margin: 10px 0;
+.col {
+  flex: 1;
+}
+
+.item-card {
+  border: 1px solid #ccc;
+  padding: 10px;
   text-align: center;
 }
 
-.item-image {
-  /* Add styles for item images */
+.item-card img {
   max-width: 100%;
   height: auto;
-}
-
-.item-description {
-  /* Add styles for item descriptions */
-  margin-top: 5px;
 }
 </style>
